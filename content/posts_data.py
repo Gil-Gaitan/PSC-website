eb815619e61f8c68852651b70938091738fff98d
Gil Gaitan IV <gaitan@fastmail.com>
2026-02-24 17:10:44 -0600

    update playlist and add a project

--- content/posts_data.py
+++ content/posts_data.py
@@ -4,6 +4,10 @@ Posts/Updates data for "Currently Cooking" section
 
 POSTS = [
     {
+        "date": "2026-02-24",
+        "text": "Updating app playlist. Started working on a new project.",
+    },
+    {
         "date": "2026-02-01",
         "text": "Updated site design. Added a personal app I've been using—converted from React to vanilla JS for Render deployment.",
     },
