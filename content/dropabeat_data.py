83f84ecf6b21c8d8a453f7b09c76dfc0a6f073ef
gil-gaitan <gaitan@fastmail.com>
2026-04-05 01:46:39 -0500

    Changed the tracks, tests good, fix the other 3 things

--- content/dropabeat_data.py
+++ content/dropabeat_data.py
@@ -1,20 +1,18 @@
 DEFAULT_TRACKS = [
-    "zhVgbZdMdb0",  # Thundercat: NPR Music Tiny Desk Concert
-    "y_YJR3hxjGI",  # jazzENIGMATIC with Jhn Adms @TheLotRadio 11-08-2025
-    "6MAzUT1YhWE",  # Fred again.. - Rooftop Live (Arun’s Roof, London)
-    "ahe9baHOWIg",  # Four Tet, Fred Again.. & Skrillex live from Times Square for @TheLotRadio
-    "a4gXBMhBsVg",  # Mic'd at MIRS with Ayanna Heaven @TheLotRadio 02-14-2026
-    "e1KbUP8PdGc",  # Salif Keita - Primpin (Live at Komyo-in Temple, Kyoto)
-    "rmgRZ0O6ZT4",  # PHOENIX - LIVE AT ACCORHOTELS ARENA PARIS (SEPT 2017)
-    "06CA5pYielY",  # Matt and Kim on July 4th 2006 at 330 Melrose
-    "ferZnZ0_rSM",  # Anderson .Paak & The Free Nationals: NPR Music Tiny Desk Concert
-    "GS2Y_CkaXP0",  # Robert Glasper - So Beautiful (Live At Capitol Studios)
-    "u-syZXHPcJE",  # Run The Jewels: NPR Music Tiny Desk Concert
-    "0tTpl77IRGA",  # Yamê - ÉBĒM Live Session
-    "Jxbsl9q1y24",  # Salif Keita - Aboubakrin | A COLORS SHOW
-    "PQF6x_FgnJ0",  # Aesop Rock - The Impossible Kid (Full Album Stream)
-    "KNrB6--KBqs",  # Aesop Rock - None Shall Pass (Official Video)
-    "6mqTG8pSvZg",  # STRFKR - Live From Brooklyn Steel
-    "uQd58-K_Xg8",  # Deltron 3030:The Instrumentals(Full Album)
-    "rFajDSmnrP8",  # Hak Baker - Luvly | A Take Away Show
+    "fKFbnhcNnjE",  # MEUTE - You & Me (Flume Remix)
+    "nj8FwAsfOsE",  # Agua - MF DOOM X TATSURO YAMASHITA [Special Edition]
+    "obZZPcjy4nY",  # ricadus - NYC messengers
+    "6S1a-CQNC3c",  # dadiamonddj *BWB* - (HD)Maps Instrumental - Yeah Yeah Yeahs
+    "AhzSJYcXtA4",  # Blanco - Fred again.., Blanco - Solo (London, November 2025)
+    "6_CFFzK2vT4",  # David William Jr. - art of a bike messenger
+    "CY3rABtKVcM",  # MF DOOM REMASTERED - Madvillain (MF DOOM) - Live at SXSW 2006 (1080p Remaster)
+    "Zx3USMspAcs",  # The Lot Radio - b4 with nourished by time @TheLotRadio 04-06-2023
+    "bvtYU6YiBEY",  # Kiosk Radio - Swimming Paul + Peterparker69 @ Kiosk Radio 13.09.2025
+    "VCV7VxZbHbA",  # The Lot Radio - John Glacier and friends @TheLotRadio 05-11-2025
+    "X0EVEBx06eQ",  # Boiler Room - Flume Boiler Room London Live set
+    "uFPPDrhHikc",  # The Lot Radio - Cat's in the Cradle with Brandon Stosuy and Son @TheLotRadio 10-30-2025
+    "OPgddNrK5o4",  # Acoolrocket DaJams - Yeah Yeah Yeahs - Soft Shock (Official Instrumental)
+    "-UH3ERVfOys",  # Boogie Belgique - Boogie Belgique - Session I (Live)
+    "t-knFuqQdGc",  # Chrystal - The Days (NOTION REMIX)
+    "h9LGmrVhwAM",  # jigitz - make believe
 ]
