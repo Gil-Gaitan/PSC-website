c7b71218ae03ca41f89976e831f0bae29688587d
gil-gaitan <gaitan@fastmail.com>
2026-04-06 20:50:06 -0500

    finish list

--- content/dropabeat_data.py
+++ content/dropabeat_data.py
@@ -2,17 +2,19 @@ DEFAULT_TRACKS = [
     "fKFbnhcNnjE",  # MEUTE - You & Me (Flume Remix)
     "nj8FwAsfOsE",  # Agua - MF DOOM X TATSURO YAMASHITA [Special Edition]
     "obZZPcjy4nY",  # ricadus - NYC messengers
-    "6S1a-CQNC3c",  # dadiamonddj *BWB* - (HD)Maps Instrumental - Yeah Yeah Yeahs
+    "6S1a-CQNC3c",  # Maps Instrumental - Yeah Yeah Yeahs
     "AhzSJYcXtA4",  # Blanco - Fred again.., Blanco - Solo (London, November 2025)
     "6_CFFzK2vT4",  # David William Jr. - art of a bike messenger
     "CY3rABtKVcM",  # MF DOOM REMASTERED - Madvillain (MF DOOM) - Live at SXSW 2006 (1080p Remaster)
-    "Zx3USMspAcs",  # The Lot Radio - b4 with nourished by time @TheLotRadio 04-06-2023
+    "Zx3USMspAcs",  # b4 with nourished by time @TheLotRadio 04-06-2023
     "bvtYU6YiBEY",  # Kiosk Radio - Swimming Paul + Peterparker69 @ Kiosk Radio 13.09.2025
-    "VCV7VxZbHbA",  # The Lot Radio - John Glacier and friends @TheLotRadio 05-11-2025
+    "VCV7VxZbHbA",  # John Glacier and friends @TheLotRadio 05-11-2025
     "X0EVEBx06eQ",  # Boiler Room - Flume Boiler Room London Live set
-    "uFPPDrhHikc",  # The Lot Radio - Cat's in the Cradle with Brandon Stosuy and Son @TheLotRadio 10-30-2025
+    "uFPPDrhHikc",  # Cat's in the Cradle with Brandon Stosuy and Son @TheLotRadio 10-30-2025
     "OPgddNrK5o4",  # Acoolrocket DaJams - Yeah Yeah Yeahs - Soft Shock (Official Instrumental)
     "-UH3ERVfOys",  # Boogie Belgique - Boogie Belgique - Session I (Live)
     "t-knFuqQdGc",  # Chrystal - The Days (NOTION REMIX)
     "h9LGmrVhwAM",  # jigitz - make believe
+    "Q692lHFaLVM",  # Nightmares on Wax - Boiler Room: London
+    "7y3CvSdW1eM",  # Flying Lotus @TheLotRadio 01-22-2026
 ]
